6713b74dcae996779f889d4f2fe458dbb9cdb594
Santosh Kumar Shaw <coolnicesantosh@gmail.com>
2016-05-11 12:26:28 +0530

    Added row and column number for debugging purpose

--- jvalid.cpp
+++ jvalid.cpp
@@ -7,10 +7,11 @@
 **/
 #include <bits/stdc++.h>
 using namespace std;
-
+int row, col;
 void error(string msg1, string msg2="")
 {
 	cout<<"Error : "<<msg1<<" "<<msg2<<endl;
+	cout<<"On line : "<<row<<" Column : "<<col<<endl;
 	exit(EXIT_FAILURE);
 }
 
@@ -33,9 +34,12 @@ int main ( int argc, char *argv[] )
 		char booli[500];		
 		char ch,prev='^',*next="{",*valid="{[\":,]}";
 		int line_no = 1, index = 0,obj=0,arr=0,i=0;
+		row = col = 1;		
 		stack <char> brac;
 		while( ( ch = fgetc(json) ) != EOF )
 		{
+			// Character Index
+			col++;
 			
 			// Scanning Valid Symbols.			
 			if( strchr(valid,ch)  || (obj && strchr(next,ch)))
@@ -43,23 +47,29 @@ int main ( int argc, char *argv[] )
 				// Checking First Valid Character.
 				if(strchr(next,ch) == NULL)
 				{
-					cout<<next<<" : "<<ch<<" : "<<prev<<endl;					
+					cout<<next<<" - "<<ch<<" - "<<prev<<endl;					
 					error("Invalid Json");
 				}				 
 				else
 				{
 					switch(ch)
 					{	
-						case '{':	if(strchr("^{,",prev)==NULL)
+						case '{':	if(strchr("^{[:,",prev)==NULL)
 									{
-										cout<<next<<" {: "<<ch<<endl;										
+										cout<<next<<" { - "<<ch<<endl;										
 										error("Invalid Json");
 									}
 									brac.push('{');
 									prev = '{';
+									obj=0;
 									next="{\"";
+									break;
+						case '[':	brac.push('[');
+									prev = '[';
+									obj = 2;
+									next="[{0123456789truefalse\"";
 									break;						
-						case '\"':	if( prev != '\"')
+						case '"':	if( prev != '\"')
 										next = "\"";
 							  		else
 									 	if( !obj)
@@ -70,14 +80,14 @@ int main ( int argc, char *argv[] )
 											obj = 0;
 										}							  			
 										else if( obj==2)
-											next=",";	
+											next=",]";	
 									prev = '"';
 									break;
 						case ':':	prev=':';
-									next="0123456789truefalse\"";
+									next="[{0123456789truefalse\"";
 									obj = 1;
 									break;
-						case ',':	if(obj==2)
+						case ',':	if(obj == 2)
 										next="0123456789truefalse\"";
 									else if(prev=='}')
 										next="{";
@@ -87,23 +97,49 @@ int main ( int argc, char *argv[] )
 									break;
 						case '}':	if(!brac.empty())	
 									{
-									brac.pop();	
-																	
-									next = "{,}";
-									prev = '}';											
+										char bracket = brac.top();
+										if( bracket != '{' )
+										{
+											error("Braces does not matches!","Invalid JSON");
+										}										
+										brac.pop();
+											next = "{,}]";
+										prev = '}';											
 									}
 									else
 									{
 										error("Fatal error");
 									}									
 									break;
-								
+						case ']':	if(!brac.empty())	
+									{
+										char bracket = brac.top();
+										if( bracket != '[' )
+										{
+											error("Braces does not matches!","Invalid JSON");
+										}										
+										brac.pop();	
+										next = ",}";
+										prev = ']';	
+										obj = 0;										
+									}
+									else
+									{
+										error("Fatal error");
+									}									
+									break;		
 					}
 
 					
 				}
-			}
 
+			}
+			else if(ch=='\n')
+			{
+				row++;
+				col = 1;			
+			}
+			
 		}
 		if(brac.empty())
 			cout<<"ALL OKK!";
